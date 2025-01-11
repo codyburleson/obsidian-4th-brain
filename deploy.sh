39cfd58f0cab80d72d1251fe17d694c6691afc7d
Cody Burleson <cody.burleson@gmail.com>
2025-01-11 16:13:28 -0600

    complete issue 1

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create directory if it doesn't exist
+mkdir -p /Users/dev/repos/knowledge-base/.obsidian/plugins/4th-brain
+
+cp ./*.js /Users/dev/repos/knowledge-base/.obsidian/plugins/4th-brain/
+cp ./*.css /Users/dev/repos/knowledge-base/.obsidian/plugins/4th-brain/
+cp ./manifest.json /Users/dev/repos/knowledge-base/.obsidian/plugins/4th-brain/
+cp ./.hotreload /Users/dev/repos/knowledge-base/.obsidian/plugins/4th-brain/
