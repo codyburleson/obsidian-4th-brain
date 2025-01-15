b218467ec4a863ff887d8fbec7ca661c9f8189b0
Cody Burleson <cody.burleson@gmail.com>
2025-01-15 05:59:38 -0700

    add session handling to SupabaseService

--- main.ts
+++ main.ts
@@ -1,239 +1,267 @@
 import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-    TFile
+  App,
+  Editor,
+  MarkdownView,
+  Modal,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  TFile,
 } from "obsidian";
 import { SupabaseService } from "./src/supabase-service";
+import { Database, Tables, Enums } from "./src/types/supabase";
 
 // Remember to rename these classes and interfaces!
 
 export interface MyPluginSettings {
-	supabaseUrl: string;
-	supabaseAnonKey: string;
-	supabaseEmail: string;
-	supabasePassword: string;
+  supabaseUrl: string;
+  supabaseAnonKey: string;
+  supabaseEmail: string;
+  supabasePassword: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	supabaseUrl: "",
-	supabaseAnonKey: "",
-	supabaseEmail: "",
-	supabasePassword: "",
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  supabaseEmail: "",
+  supabasePassword: "",
 };
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
+  async onload() {
+    await this.loadSettings();
 
-		await this.loadSettings();
+    this.registerEvent(
+      this.app.workspace.on("file-menu", (menu, file) => {
+        if (
+          file instanceof TFile &&
+          (file.extension === "canvas" || file.extension === "md")
+        ) {
+          menu.addItem((item) => {
+            item.setTitle("Sync With Supabase").onClick(async () => {
+              try {
+                console.log(">> 4thBrain > Sync With Supabase");
+                console.log(">> 4thBrain > file.path: ", file.path);
+                console.log(">> 4thBrain > file.basename: ", file.basename);
+                console.log(">> 4thBrain > file.extension: ", file.extension);
 
-
-        this.registerEvent(this.app.workspace.on(
-            'file-menu',
-            (menu, file) => {
-                if (file instanceof TFile && (file.extension === 'canvas' || file.extension === 'md')) {
-                    menu.addItem((item) => {
-                        item.setTitle('Sync With Supabase').onClick(async () => {
-                            console.log('>> 4thBrain > Sync With Supabase');
-                        });
-                    });
-                }
-            }
-        ));
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				console.debug(
-					"-- main.onLoad() RibbonIcon MouseEvent handler triggered..."
-				);
-				let supabaseService = new SupabaseService(
-					this.settings.supabaseUrl,
-					this.settings.supabaseAnonKey
-				);
-				supabaseService.signInWithPassword(
-					this.settings.supabaseEmail,
-					this.settings.supabasePassword
+                const supabaseService = new SupabaseService(
+                  this.settings.supabaseUrl,
+                  this.settings.supabaseAnonKey
                 );
 
-                let document = {
-                    id: '8b86d301-0b82-489c-b070-4349f10c5c33',
-                    version: 0, // how to make an auto-incrementing field?
-                    content: '# Hello 4',
-                    //is_latest: true,
-                    state: 'published'
-                }
-                console.debug('-- insert result:: ', supabaseService.insertDocument(document));
-                new Notice('RibbonIcon MouseEvent handler triggered ');
-			}
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
+                // This will handle checking the session and signing in if needed
+                const user = await supabaseService.ensureSession(
+                  this.settings.supabaseEmail,
+                  this.settings.supabasePassword
+                );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
+                // If we get here, we have an active session with a valid user
+                console.log(
+                  ">> 4thBrain > Session established for user:",
+                  user.email
+                );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
+                // TODO: Implement your sync logic here
+              } catch (error) {
+                console.error(">> 4thBrain > Error during sync:", error);
+                new Notice("Failed to sync with Supabase: " + error.message);
+              }
+            });
+          });
+        }
+      })
+    );
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    // This creates an icon in the left ribbon.
+    const ribbonIconEl = this.addRibbonIcon(
+      "dice",
+      "Sample Plugin",
+      (evt: MouseEvent) => {
+        console.debug(
+          "-- main.onLoad() RibbonIcon MouseEvent handler triggered..."
+        );
+        let supabaseService = new SupabaseService(
+          this.settings.supabaseUrl,
+          this.settings.supabaseAnonKey
+        );
+        supabaseService.signInWithPassword(
+          this.settings.supabaseEmail,
+          this.settings.supabasePassword
+        );
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
+        let document = {
+          id: "8b86d301-0b82-489c-b070-4349f10c5c33",
+          version: 0, // how to make an auto-incrementing field?
+          content: "# Hello 4",
+          //is_latest: true,
+          state: "published",
+        };
+        console.debug(
+          "-- insert result:: ",
+          supabaseService.insertDocument(document)
+        );
+        new Notice("RibbonIcon MouseEvent handler triggered ");
+      }
+    );
+    // Perform additional things with the ribbon
+    ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
-	}
+    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText("Status Bar Text");
 
-	onunload() {}
+    // This adds a simple command that can be triggered anywhere
+    this.addCommand({
+      id: "open-sample-modal-simple",
+      name: "Open sample modal (simple)",
+      callback: () => {
+        new SampleModal(this.app).open();
+      },
+    });
+    // This adds an editor command that can perform some operation on the current editor instance
+    this.addCommand({
+      id: "sample-editor-command",
+      name: "Sample editor command",
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        console.log(editor.getSelection());
+        editor.replaceSelection("Sample Editor Command");
+      },
+    });
+    // This adds a complex command that can check whether the current state of the app allows execution of the command
+    this.addCommand({
+      id: "open-sample-modal-complex",
+      name: "Open sample modal (complex)",
+      checkCallback: (checking: boolean) => {
+        // Conditions to check
+        const markdownView =
+          this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (markdownView) {
+          // If checking is true, we're simply "checking" if the command can be run.
+          // If checking is false, then we want to actually perform the operation.
+          if (!checking) {
+            new SampleModal(this.app).open();
+          }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+          // This command will only show up in Command Palette when the check function returns true
+          return true;
+        }
+      },
+    });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+
+    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+    // Using this function will automatically remove the event listener when this plugin is disabled.
+    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+      console.log("click", evt);
+    });
+
+    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    this.registerInterval(
+      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+    );
+
+    console.log("4th Brain Plugin v0.0.1-cb3 loaded");
+  }
+
+  onunload() {}
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.setText("Woah!");
+  }
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName("Supabase URL")
-			.setDesc("The URL of your Supabase project")
-			.addText((text) =>
-				text
-					.setPlaceholder("project URL")
-					.setValue(this.plugin.settings.supabaseUrl)
-					.onChange(async (value) => {
-						this.plugin.settings.supabaseUrl = value;
-						await this.plugin.saveSettings();
-					})
-			);
+    new Setting(containerEl)
+      .setName("Supabase URL")
+      .setDesc("The URL of your Supabase project")
+      .addText((text) =>
+        text
+          .setPlaceholder("project URL")
+          .setValue(this.plugin.settings.supabaseUrl)
+          .onChange(async (value) => {
+            this.plugin.settings.supabaseUrl = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName("Supabase Anon Key")
-			.setDesc(
-				"The Supabase project anon key. If you are hosting Supabase yourself, you can fing this in the .env file (ANON_KEY in supabase/docker/.env)"
-			)
-			.addText((text) =>
-				text
-					.setPlaceholder("anon key")
-					.setValue(this.plugin.settings.supabaseAnonKey)
-					.onChange(async (value) => {
-						this.plugin.settings.supabaseAnonKey = value;
-						await this.plugin.saveSettings();
-					})
-			);
+    new Setting(containerEl)
+      .setName("Supabase Anon Key")
+      .setDesc(
+        "The Supabase project anon key. If you are hosting Supabase yourself, you can fing this in the .env file (ANON_KEY in supabase/docker/.env)"
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder("anon key")
+          .setValue(this.plugin.settings.supabaseAnonKey)
+          .onChange(async (value) => {
+            this.plugin.settings.supabaseAnonKey = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName("Supabase Email")
-			.setDesc("Supabase signin email")
-			.addText((text) =>
-				text
-					.setPlaceholder("signin email")
-					.setValue(this.plugin.settings.supabaseEmail)
-					.onChange(async (value) => {
-						this.plugin.settings.supabaseEmail = value;
-						await this.plugin.saveSettings();
-					})
-			);
+    new Setting(containerEl)
+      .setName("Supabase Email")
+      .setDesc("Supabase signin email")
+      .addText((text) =>
+        text
+          .setPlaceholder("signin email")
+          .setValue(this.plugin.settings.supabaseEmail)
+          .onChange(async (value) => {
+            this.plugin.settings.supabaseEmail = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName("Supabase Password")
-			.setDesc(
-				"Supabase signin password"
-			)
-			.addText((text) =>
-				text
-					.setPlaceholder("anon key")
-					.setValue(this.plugin.settings.supabasePassword)
-					.onChange(async (value) => {
-						this.plugin.settings.supabasePassword = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
+    new Setting(containerEl)
+      .setName("Supabase Password")
+      .setDesc("Supabase signin password")
+      .addText((text) =>
+        text
+          .setPlaceholder("anon key")
+          .setValue(this.plugin.settings.supabasePassword)
+          .onChange(async (value) => {
+            this.plugin.settings.supabasePassword = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
--- src/supabase-service.ts
+++ src/supabase-service.ts
@@ -1,52 +1,121 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import {
+  createClient,
+  SupabaseClient,
+  User,
+} from "@supabase/supabase-js";
 import { Database } from "./types/supabase";
 
 export class SupabaseService {
-	private supabase: SupabaseClient;
+  private readonly supabase: SupabaseClient;
 
-	constructor(supabaseUrl: string, supabaseAnonKey: string) {
-		// https://www.restack.io/docs/supabase-knowledge-supabase-generate-types
-		// The <Database> type ensures that the Supabase client is aware of the database schema,
-		// providing autocompletion and type checking for database operations.
-		this.supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
-	}
+  constructor(supabaseUrl: string, supabaseAnonKey: string) {
+    // https://www.restack.io/docs/supabase-knowledge-supabase-generate-types
+    // The <Database> type ensures that the Supabase client is aware of the database schema,
+    // providing autocompletion and type checking for database operations.
+    this.supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+  }
 
-	async signInWithPassword(email: string, password: string) {
-		let { data, error } = await this.supabase.auth.signInWithPassword({
-			email: email,
-			password: password,
-		});
-		console.debug(">> SupabaseService.signInWithPassword: ", data, error);
-	}
+  /**
+   * Checks if there is an active session
+   * @returns Promise<boolean> indicating if there's an active session
+   */
+  async hasActiveSession(): Promise<boolean> {
+    try {
+      const {
+        data: { session },
+        error,
+      } = await this.supabase.auth.getSession();
+      if (error) throw error;
+      return session !== null;
+    } catch (error) {
+      console.error("Error checking session:", error);
+      return false;
+    }
+  }
 
-	async getCurrentUser() {
-		const {
-			data: { user },
-			error,
-		} = await this.supabase.auth.getUser();
-		if (error) throw new Error(`Failed to get user: ${error.message}`);
-		return user;
-	}
+  async signInWithPassword(
+    email: string,
+    password: string
+  ): Promise<User | null> {
+    console.debug(">> SupabaseService.signInWithPassword() > email: ", email);
+    try {
+      const {
+        data: { user },
+        error,
+      } = await this.supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+      if (error) throw error;
+      console.debug(">> SupabaseService.signInWithPassword: ", user);
+      return user;
+    } catch (error) {
+      console.error("Error signing in:", error);
+      throw error;
+    }
+  }
 
-    // TODO: replace with document type or interface...
-	async insertDocument(document: any) {
+  async signOut() {
+    console.debug(">> SupabaseService.signOut()");
+    const { error } = await this.supabase.auth.signOut();
+    if (error) throw new Error(`Failed to sign out: ${error.message}`);
+  }
 
-		const user = await this.getCurrentUser();
-        console.debug('-- SupabaseService.insertDocument: user: ', user);
-		if (!user) throw new Error("User not authenticated");
+  async getUser() {
+    console.debug(">> SupabaseService.getUser()");
+    const {
+      data: { user },
+      error,
+    } = await this.supabase.auth.getUser();
+    if (error) {
+      console.error(">> SupabaseService.getUser: error: ", error);
+      throw new Error(`Failed to get user: ${error.message}`);
+    }
+    return user;
+  }
 
-		const { data, error } = await this.supabase
-			.from("documents")
-			.insert([
-				{
-					...document,
-					created_by: user.id, // Associate document with authenticated user
-				},
-			])
-			.select();
+  /**
+   * Ensures there is an active session, signing in if necessary
+   * @param email user's email for sign in if needed
+   * @param password user's password for sign in if needed
+   * @returns Promise<User> the current user
+   */
+  async ensureSession(email: string, password: string): Promise<User> {
+    try {
+      const hasSession = await this.hasActiveSession();
+      if (!hasSession) {
+        const user = await this.signInWithPassword(email, password);
+        if (!user) throw new Error("Failed to establish session");
+        return user;
+      }
 
-		if (error)
-			throw new Error(`Error inserting document: ${error.message}`);
-		return data;
-	}
+      const user = await this.getUser();
+      if (!user) throw new Error("No user found despite active session");
+      return user;
+    } catch (error) {
+      console.error("Error ensuring session:", error);
+      throw error;
+    }
+  }
+
+  // TODO: replace with document type or interface...
+  async insertDocument(document: any) {
+    console.debug(">> SupabaseService.insertDocument: document: ", document);
+    const user = await this.getUser();
+    console.debug("-- SupabaseService.insertDocument: user: ", user);
+    if (!user) throw new Error("User not authenticated");
+
+    const { data, error } = await this.supabase
+      .from("documents")
+      .insert([
+        {
+          ...document,
+          created_by: user.id, // Associate document with authenticated user
+        },
+      ])
+      .select();
+
+    if (error) throw new Error(`Error inserting document: ${error.message}`);
+    return data;
+  }
 }
